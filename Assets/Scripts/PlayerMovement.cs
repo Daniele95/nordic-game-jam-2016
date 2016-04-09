25597f206351f08f469cc5325b4a42cb52912bc8
xzores <jakobfu@hotmail.com>
2016-04-09 10:57:15 +0200

    Uploading the first project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+    public int ControllerID = 1;
+    [SerializeField]
+    private Vector3[] SpawnPoints = new Vector3[4];
+    [SerializeField]
+    private GameObject Projectile;
+    [SerializeField]
+    private float ProjectileSpeed;
+    [SerializeField]
+    private float speedVer;
+    [SerializeField]
+    private float speedHor;
+    [SerializeField]
+    private float JumpForce;
+    private Rigidbody body;
+    private bool grounded = false;
+
+
+    void Start () {
+        body = gameObject.GetComponent<Rigidbody>();
+    }
+	
+	void Update () {
+
+        grounded = isGrounded();
+
+        body.MovePosition(new Vector3(Input.GetAxis("Horizontal" + ControllerID) * speedHor * Time.deltaTime, 0) + transform.position);
+
+        bool InputX = false;
+        bool InputE = false;
+
+        if (ControllerID == 1) {
+            InputX = Input.GetKeyDown(KeyCode.Joystick1Button0);
+            InputE = Input.GetKeyDown(KeyCode.Joystick1Button1);
+        }
+        if (ControllerID == 2)
+        {
+            InputX = Input.GetKeyDown(KeyCode.Joystick2Button0);
+            InputE = Input.GetKeyDown(KeyCode.Joystick1Button1);
+        }
+        if (ControllerID == 3)
+        {
+            InputX = Input.GetKeyDown(KeyCode.Joystick3Button0);
+            InputE = Input.GetKeyDown(KeyCode.Joystick1Button1);
+        }
+        if (ControllerID == 4)
+        {
+            InputX = Input.GetKeyDown(KeyCode.Joystick4Button0);
+            InputE = Input.GetKeyDown(KeyCode.Joystick1Button1);
+        }
+
+        if ((Input.GetKeyDown(KeyCode.W) || InputX) && grounded) {
+            body.velocity = new Vector3(body.velocity.x, JumpForce, body.velocity.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || InputE)
+        {
+            Shoot();
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wisp") {
+            Die();
+        }
+    }
+
+    void Die() {
+        transform.position = SpawnPoints[Random.Range(0,4)];
+    }
+
+    void Shoot() {
+        GameObject g = Instantiate(Projectile, new Vector3(transform.position.x, transform.position.y, transform.position.z) + transform.forward * 1.1f, Quaternion.identity) as GameObject;
+        g.GetComponent<Rigidbody>().velocity = transform.forward * ProjectileSpeed;
+        g.transform.LookAt(transform.position + transform.forward * 5);
+    }
+
+    bool isGrounded() {
+        return Physics.Raycast(transform.position, Vector3.down, 0.6f);
+    }
+}
---
+++ Assets/Scripts/Wisp.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wisp : MonoBehaviour {
+
+    private int color = 1;
+    Material newMaterial;
+
+    void Start () {
+        MeshRenderer gameObjectRenderer = gameObject.GetComponent<MeshRenderer>();
+
+        newMaterial = new Material("my mat");
+
+        newMaterial.color = Color.white;
+        gameObjectRenderer.material = newMaterial;
+    }
+
+	void Update () {
+
+        if (color == 0)
+        {
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+            gameObject.GetComponent<Rigidbody>().drag = 5;
+            newMaterial.color = Color.white;
+            gameObject.tag = "Untagged";
+        }
+        else {
+            gameObject.tag = "wisp";
+            newMaterial.color = Color.white;
+        }
+
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        color = 0;
+    }
+}
