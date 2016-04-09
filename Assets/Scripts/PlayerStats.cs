e1e078aa97216bcfac5871093467c24ea94f80db
xzores <jakobfu@hotmail.com>
2016-04-09 17:26:19 +0200

    Please enter the commit message for your changes. Lines starting

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats {
+
+    public static int P1S;
+    public static int P2S;
+    public static int P3S;
+    public static int P4S;
+
+}
