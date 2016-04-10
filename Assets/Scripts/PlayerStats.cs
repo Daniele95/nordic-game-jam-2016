59e180783ff0a6682b4dcdeadc0c7691ccc4f277
Renato <lol@asd.com>
2016-04-10 11:19:27 +0200

    endgamescreen

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -8,6 +8,9 @@ public class PlayerStats : MonoBehaviour {
 
     public float RemaningTime;
 	public Text TimeRemaining;
+	public Image EndGame;
+	public float transition_time;
+	public float end_game_time;
 
     public int P1S = 0;
     public int P2S = 0;
@@ -21,8 +24,13 @@ public class PlayerStats : MonoBehaviour {
 		int showing = (int) Mathf.Round (RemaningTime);
 		TimeRemaining.text = showing.ToString();
 		if (RemaningTime < 0.0f) {
-			SceneManager.LoadScene ( "levelblueprint" );
-			
+			if (RemaningTime >= -1.0) {
+				Color c = EndGame.color;
+				c.a = Mathf.Lerp (0.0f, 1.0f, -RemaningTime);
+				EndGame.color = c;
+			}
+			if (RemaningTime < -5.0)
+				SceneManager.LoadScene ("levelblueprint");
 		}
 	}
 
