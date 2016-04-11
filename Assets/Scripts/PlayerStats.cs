3f5b707da471ac9244aea6c1048a256a5207aa10
xzores <jakobfu@hotmail.com>
2016-04-11 08:32:27 +0200

    the last changes

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 public class PlayerStats : MonoBehaviour {
 	public static PlayerStats Statistics = new PlayerStats();
 
+    public GameObject RedWin;
+    public GameObject BlueWin;
+    public GameObject GreenWin;
+    public GameObject YellowWin;
+
     public float RemaningTime;
 	public Text TimeRemaining;
 	public Image EndGame;
@@ -21,22 +26,50 @@ public class PlayerStats : MonoBehaviour {
 	void Update()
 	{
 		RemaningTime -= Time.deltaTime;
-		int showing = (int) Mathf.Round (RemaningTime);
+		int showing = Mathf.Max((int) Mathf.Round (RemaningTime), 0);
 		TimeRemaining.text = showing.ToString();
+
 		if (RemaningTime < 0.0f) {
 			if (RemaningTime >= -1.0) {
                 Statistics = new PlayerStats();
-                Color c = EndGame.color;
-				c.a = Mathf.Lerp (0.0f, 1.0f, -RemaningTime);
-				EndGame.color = c;
-			}
-			if (RemaningTime < -5.0)
-                    Statistics = new PlayerStats();
-                    P1S = 0;
-                    P2S = 0;
-                    P3S = 0;
-                    P4S = 0;
-                    SceneManager.LoadScene ("levelblueprint");
+
+                float f = Mathf.Max(P1S, Mathf.Max(P2S, Mathf.Max(P3S, P4S)));
+                int w = 0;
+
+                if (f == P1S)
+                    w = 1;
+
+                if (f == P2S)
+                    w = 2;
+
+                if (f == P3S)
+                    w = 3;
+
+                if (f == P4S)
+                    w = 4;
+
+                if (w == 1)
+                    RedWin.SetActive(true);
+
+                if (w == 2)
+                    YellowWin.SetActive(true);
+
+                if (w == 3)
+                    GreenWin.SetActive(true);
+
+                if (w == 4)
+                    BlueWin.SetActive(true);
+            }
+            if (RemaningTime < -5.0)
+            {
+
+                Statistics = new PlayerStats();
+                P1S = 0;
+                P2S = 0;
+                P3S = 0;
+                P4S = 0;
+                SceneManager.LoadScene("levelblueprint");
+            }
 		}
 	}
 
